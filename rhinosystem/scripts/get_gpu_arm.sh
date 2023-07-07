fb41e951aa9a3a32ac6f5c195c56953a8b2b47c2
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-07-06 21:25:06 -0500

    fix(get_gpu_arm): add pipefail

--- rhinosystem/scripts/get_gpu_arm.sh
+++ rhinosystem/scripts/get_gpu_arm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -o pipefail
 glxinfo | grep Device | tail -n1 | awk '{print $2}'
 
 # will print result like `Mali G52` or `Mali T860` - package "mesa-utils" is required for `glxinfo`
