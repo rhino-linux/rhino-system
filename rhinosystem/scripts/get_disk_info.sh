365a7de0c7f1df406303b562cdecb226784b2e58
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2023-07-31 15:39:57 -0500

    Quote because shellcheck

--- rhinosystem/scripts/get_disk_info.sh
+++ rhinosystem/scripts/get_disk_info.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 total_space_gb=$(echo "scale=2; $(df --output=size --total | awk 'END {print $1}') / 1000000" | bc)
-if [ $(echo "$total_space_gb > 1000" | bc -l) != 0 ]; then
+if [ "$(echo "$total_space_gb > 1000" | bc -l)" != 0 ]; then
   printf "%.1f TB\n" "$(echo "scale=2; $total_space_gb / 1000" | bc)"
 else
   printf "%.0f GB\n" "${total_space_gb}"
