774c264ebad097b8f4ede89e329548de433dfa6e
oklopfer <104327997+oklopfer@users.noreply.github.com>
2023-07-31 16:50:52 -0400

    make it by bytes

--- rhinosystem/scripts/get_disk_info.sh
+++ rhinosystem/scripts/get_disk_info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-total_space_gb=$(echo "scale=2; $(df --output=size --total | awk 'END {print $1}') / 1000000" | bc)
+total_space_gb=$(echo "scale=2; $(df --output=size --total | awk 'END {print $1}') / 1000000 * 1.024" | bc)
 if [ "$(echo "$total_space_gb > 1000" | bc -l)" != 0 ]; then
   printf "%.1f TB\n" "$(echo "scale=2; $total_space_gb / 1000" | bc)"
 else
