ad3c5c949560f4e220f4527d33bfbf528ef1d79a
Elsie <hwengerstickel@protonmail.com>
2023-07-31 16:11:58 -0500

    perf(get_disk_info): switch to dash (#16)
    * perf(get_disk_info): switch to dash
    
    * Quote because shellcheck
    
    * make it by bytes
    
    ---------
    
    Co-authored-by: oklopfer <104327997+oklopfer@users.noreply.github.com>

--- rhinosystem/scripts/get_disk_info.sh
+++ rhinosystem/scripts/get_disk_info.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-total_space=$(df --total --block-size=1K | grep total | awk '{print $2}')
-total_space_gb=$(echo "scale=2; $total_space / 1000000" | bc)
-if (( $(echo "$total_space_gb > 1000" | bc -l) )); then
-  total_space_tb=$(echo "scale=2; $total_space_gb / 1000" | bc)
-  printf "%.1f TB\n" "${total_space_tb}"
+#!/bin/sh
+total_space_gb=$(echo "scale=2; $(df --output=size --total | awk 'END {print $1}') / 1000000 * 1.024" | bc)
+if [ "$(echo "$total_space_gb > 1000" | bc -l)" != 0 ]; then
+  printf "%.1f TB\n" "$(echo "scale=2; $total_space_gb / 1000" | bc)"
 else
   printf "%.0f GB\n" "${total_space_gb}"
 fi
